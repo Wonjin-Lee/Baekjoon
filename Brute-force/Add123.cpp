39eda211d7b804068162840ada1ecf7bea70b35c
spacewhales <claude17@naver.com>
2019-08-14 23:43:40 +0900

    Recursion(1) : Add123

---
+++ Brute-force/Add123.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int go(int sum, int goal)
+{
+    if(sum > goal)
+        return 0;
+
+    if(sum == goal)
+        return 1;
+
+    int now = 0;
+
+    for(int i=1; i <= 3; i++)
+    {
+        now += go(sum + i, goal);
+    }
+    return now;
+}
+
+int main()
+{
+    int testCase;
+    cin >> testCase;
+
+    for(int i=0; i < testCase; i++)
+    {
+        int num;
+        cin >> num;
+
+        cout << go(0, num) << '\n';
+    }
+}
